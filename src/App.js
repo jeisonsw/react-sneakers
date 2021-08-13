c3de85f568488397797f38dfedb0c162effdeb01
unknown <jeisonsw>
2021-08-13 14:15:55 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import Card from './components/Card';
-import Header from './components/Header';
-import Drawer from './components/Drawer';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
+const arr = [
+  { title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: '/img/sneakers/1.jpg', },
+  { title: "Мужские Кроссовки Nike Air Max 270", price: 15600, imageUrl: '/img/sneakers/2.jpg', },
+  { title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imageUrl: '/img/sneakers/3.jpg', },
+  { title: "Кроссовки Puma X Aka Boku Future Rider", price: 8500, imageUrl: '/img/sneakers/4.jpg', },
+];
 
 function App() {
   return (
     <div className="wrapper clear">
-     <Drawer />
-     <Header />
-      <div className = "content p-40">
-        <div className = "d-flex align-center justify-between mb-40">
-        <h1>Все кроссовки</h1>
-        <div className = "search-block d-flex">
-          <img src="/img/search.svg" alt="Search" />
-          <input placeholder = "Поиск..." />
-        </div>
-        </div>
-
-        <div className = "d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
         </div>
 
+        <div className="d-flex">
+        {arr.map((obj) => (
+            <Card title = {obj.title} price = {obj.price} imageUrl = {obj.imageUrl} />
+        ))}
+         
+        </div>
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
-function Card() {
+function Card(props) {
+
+  const onClickButton = () => {
+    alert (props.title);
+  }
+
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 грн</b>
+          <b>{props.price} грн.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick = {onClickButton}>
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
